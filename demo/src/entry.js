117404726d89a5046dd005fdfae482f2e86033a2
心叶 <yelloxing@gmail.com>
2019-10-28 22:58:29 +0800

    添加水印

--- demo/src/entry.js
+++ demo/src/entry.js
@@ -9,6 +9,10 @@ import '@yelloxing/normalize.css';
 // 引入主页面
 import './page/App';
 
+// 添加水印(懒加载)
+let watermark = resolve => require(['./service/watermark'], resolve);
+new Promise(resolve => watermark(resolve)).then(callback => callback.default("Abandon 用例"));
+
 //根对象
 window.vm = new Abandon({
 
